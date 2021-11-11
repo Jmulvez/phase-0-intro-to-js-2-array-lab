fa3317ed0183b2896ca1373655a4655c773a69b4
John <johnmulvey48@yahoo.com>
2021-11-11 07:45:41 -0500

    git@github.com:Jmulvez/phase-0-intro-to-js-2-array-lab.git

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,14 +4,14 @@ describe('index.js', function () {
       expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
     });
   });
-
+  const cats = {["Milo", "Otis", "Garfield"]
+  }
   describe('Array functions', function () {
     beforeEach(function () {
       cats.length = 0;
 
       cats.push('Milo', 'Otis', 'Garfield');
     });
-
     describe('destructivelyAppendCat(name)', function () {
       it('appends a cat to the end of the cats array', function () {
         destructivelyAppendCat('Ralph');
